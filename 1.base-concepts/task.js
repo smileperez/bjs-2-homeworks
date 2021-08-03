8f8b57279302e5817baae151ad3951f87252e3a7
Andrey Efimenko <aa.efimenko@yandex.ru>
2021-08-04 02:04:40 +0300

    Решена 2-ая задача

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,8 +1,8 @@
-'use strict';
+'use strict'; // Активируем строгий режим
 
 function solveEquation(a, b, c) {
   let arr = [];
-  let d = b ** 2 - 4 * a * c; //Ищем дискриминант
+  let d = b ** 2 - 4 * a * c; // Ищем дискриминант квадратного уравнения
   console.log(`Дискриминант равен = ${d}`);
 
   if (d < 0) {
@@ -15,14 +15,78 @@ function solveEquation(a, b, c) {
     arr.push((-b - Math.sqrt(d)) / (2 * a));
     return arr; // выводим из функции массив с двумя значениями X при d > 0
   } else {
-    console.log(`Произошла ошибка`);
+    console.log(`Произошла ошибка`); // На всякий случай оставляем вывод ошибки
   }
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
-  let totalAmount;
+  
+  // ПРОВЕРКА ВВЕДЕННЫХ ЗНАЧЕНИЙ
+  if (isNaN(Number(percent)) == true || isNaN(Number(contribution)) == true || isNaN(Number(amount)) == true) {
+    console.log(`Ошибка! Введены не корректные данные!`);
+  } else {
+    // ПРИВОДИМ ТИПЫ ДАННЫХ В ПОРЯДОК
+    percent = Number(percent);
+    contribution = Number(contribution);
+    amount = Number(amount);
+     
+    // СЧИТАЕМ ОБЩИЙ СРОК ИПОТЕКИ В МЕСЯЦАХ
+    let currentDate = new Date(); // Текущая дата
+    let creditTerm; // Срок импотеки в месяцах
 
-  // код для задачи №2 писать здесь
+    // Функция получения срока ипотеки между текущей датой и датой окончания ипотеки (количество месяцев)
+    function calcCreditTerm(nowDate, targetDate) {
+      let term1 = 11 - nowDate.getMonth(); // Кол-во месяцев от сегодня до конца текущего года
+      let term2 = (targetDate.getFullYear() - nowDate.getFullYear() - 1) * 12; // Кол-во месяцев в полных годах
+      let term3 = targetDate.getMonth() + 1; // Кол-во месяцев от начала года, до конца срока ипотеки
 
-  return totalAmount;
+      if (targetDate < nowDate) { // Проверка на глупость
+        console.log(`Ошибка!Конечная дата ипотеки не может быть раньше сегодняшней даты!`)
+
+      } else if (targetDate.getFullYear() - nowDate.getFullYear() === 0) { // Кол-во месяцев, если все происходит внутри одного календарного года
+        creditTerm = creditTerm = targetDate.getMonth() - nowDate.getMonth();
+        console.log(`Всего месяцев до конца ипотеки: ${creditTerm}`)
+
+      } else if (targetDate.getFullYear() - nowDate.getFullYear() === 1) { // Кол-во месяцев, если все происходит внутри двух календарных лет
+        creditTerm = term1 + term3;
+        console.log(`Всего месяцев до конца ипотеки: ${creditTerm}`)
+
+      } else if (targetDate.getFullYear() - nowDate.getFullYear() > 1) { // Кол-во месяцев, если все срок больше календарных 2 лет
+        creditTerm = term1 + term2 + term3;
+        console.log(`Всего месяцев до конца ипотеки: ${creditTerm}`)
+      }
+      return creditTerm;
+    }
+
+    // СЧИТАЕМ ОБЩИЙ СРОК ИПОТЕКИ В МЕСЯЦАХ
+    calcCreditTerm(currentDate, date); 
+      
+    // СЧИТАЕМ ОБЩУЮ СУММУ КРЕДИТА
+    let totalAmount; // Общая сумма, которую заплатит Клиент
+
+    if (amount === contribution) { // Проверяем, а не хочет ли Клиент протупить и дать первую сумму равной сумме кредита?
+      totalAmount = 0;
+      return totalAmount;
+
+    } else {
+      let loanBody = amount - contribution; // Тело кредита
+      let paymentPerMonth; // Платеж в месяц
+      let percentPerMonth; // Процентная ставка в месяц
+
+      // Считаем процентную ставку в месяц
+      percentPerMonth = percent / 100 / 12;
+
+      // Считаем платеж в месяц
+      paymentPerMonth = loanBody * (percentPerMonth + percentPerMonth / (((1 + percentPerMonth) ** creditTerm) - 1));
+      paymentPerMonth = paymentPerMonth;
+      console.log(`Платеж в месяц: ${paymentPerMonth}`);
+
+      // Считаем общую сумму кредита
+      totalAmount = paymentPerMonth * creditTerm;
+      totalAmount = totalAmount.toFixed(2);
+      console.log(`Полная стоимость кредита: ${totalAmount}`)
+
+      return totalAmount;
+    } 
+  }
 }
