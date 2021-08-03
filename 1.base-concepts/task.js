264a7a80d7aa322663e3393e0a43f12e2889f1a6
Andrey Efimenko <aa.efimenko@yandex.ru>
2021-08-03 22:39:15 +0300

    Решена первая задача. Базовые тесты пройдены

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,6 +1,22 @@
+'use strict';
+
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
+  let arr = [];
+  let d = b ** 2 - 4 * a * c; //Ищем дискриминант
+  console.log(`Дискриминант равен = ${d}`);
+
+  if (d < 0) {
+    console.log(arr);
+    return arr; // array
+  } else if (d === 0) {
+    arr.push(-b / (2 * a));
+    console.log(arr);
+  } else if (d > 0) {
+    arr.push((-b + Math.sqrt(d)) / (2 * a));
+    arr.push((-b - Math.sqrt(d)) / (2 * a));
+    console.log(arr);
+  }
+
   return arr; // array
 }
 
