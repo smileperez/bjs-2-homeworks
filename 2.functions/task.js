947821c8cbb2fb68362362a5633b856bf7bd9c52
Andrey Efimenko <aa.efimenko@yandex.ru>
2021-08-06 22:46:39 +0300

    Решена задача 2-1

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,11 +1,37 @@
 // Задание 1
 function getArrayParams(arr) {
-  let min,max,sum,avg;
+  let min = Infinity;
+  let max = -Infinity;
+  let sum = 0;
+  let avg;
 
-  // Ваш код
+  console.log(arr);
 
+  for (let i = 0; i < arr.length; i++) {
+    
+    // Определяем минимальное значение
+    if (arr[i] < min) {
+      min = arr[i];
+      console.log(`min теперь ${arr[i]}`);
+    } 
+    
+    // Определяем максимальное значение
+    if (arr[i] > max) {
+      max = arr[i];
+      console.log(`max теперь ${arr[i]}`);
+    }
+    
+    // Рассчитываем сумму массива
+    sum = sum + arr[i];
+  }
+
+  // Рассчитываем среднее значение массива
+  avg = Number((sum / arr.length).toFixed(2));
+      
   return { min:min, max:max, avg:avg };
 }
+
+
 
 // Задание 2
 function worker(arr) {
