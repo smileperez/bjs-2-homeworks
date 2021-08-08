1ac3a7ad9eb5f8621f9f91b8c484c974ca74e38a
Andrey Efimenko <aa.efimenko@yandex.ru>
2021-08-08 11:15:03 +0300

    Решена задача 3-2

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,11 +1,11 @@
+// Задача 3-1
 function compareArrays(arr1, arr2) {
+  // Сравнили длину массива - если не равны то выдать false, далее если длина одинаковая то сравниваем конкретные значения.
   return arr1.length == arr2.length && arr1.every((item, idx) => item === arr2[idx]);
 }
 
+// Задача 3-2
 function advancedFilter(arr) {
-  let resultArr;
-
-  // Ваш код
-
-  return resultArr; // array
+  // Фильтр на положительные, следом фильтр на кратные, следом изменение в 10 раз.
+  return arr.filter((item, idx) => item > 0).filter((item, idx) => item % 3 === 0).map((item, idx) => item * 10);
 }
