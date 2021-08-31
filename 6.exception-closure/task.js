e158d78da3bd798e50884e17f01ab5d31bd6d118
Andrey Efimenko <aa.efimenko@yandex.ru>
2021-08-31 22:56:04 +0300

    Решена задача 2-1

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -19,3 +19,29 @@ const validateCount = (parse) => {
 
 
 // ЗАДАЧА 2
+class Triangle {
+
+    constructor (a, b, c) {
+        if (a + b > c && a + c > b && b + c > a) {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        } else {
+            throw new Error("Треугольник с такими сторонами не существует")
+        }
+    }
+
+    getPerimeter() {
+        return this.a + this.b + this.c;
+    }
+
+    getArea() {
+        let p = this.getPerimeter() / 2;
+        let s = Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c));
+        return Number(s.toFixed(3));
+    }
+}
+
+const getTriangle = (a, b, c) => {
+
+}
