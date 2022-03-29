636be5dc4e93f17b65de9914d12cc36f405f321b
Andrey Efimenko <aa.efimenko@efimen.ru>
2022-03-29 23:44:19 +0300

    some change 2

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -9,24 +9,28 @@ function cachingDecoratorNew(func) {
     console.log(`Принимаем новую порцию хеша: ${hash}`);
     console.log(`Текущий размер кеша: ${cache.length}`);
 
+
     let idx = cache.findIndex((item) => item.hash === hash) // ищем элемент, хэш которого равен нашему хэшу
     
     if (idx !== -1 ) { // если элемент найден
       console.log(`Найден кеш. Результат из кэша: ${cache[idx].result}`)// индекс нам известен, по индексу в массиве лежит объект, как получить нужное значение?
       return "Из кэша: " + cache[idx].result;
+
     } else {
       console.log(`В кэше результата нет - придётся считать`);
       let result = func(...args); // в кэше результата нет - придётся считать
       cache.push({hash, result}); // добавляем элемент с правильной структурой
       console.log(`Производим вычисление: ${result}`);
       console.log("Добавляем вычисленный результат в кеш");
-      
+
       if (cache.length > 5) { 
         cache.splice(0, 1); // если слишком много элементов в кэше надо удалить самый старый (первый)
         console.log("Кеш переполнен, удаляем самый старый кеш")
       }
-      return "Вычисляем: " + result;  
+
+      return "Вычисляем: " + result; 
     }
+    
   }
   
   return wrapper;
